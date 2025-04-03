f103c34ef88c2de7703cb0fffb907d08d6237511
iloveschooljk <103543624+iloveschooljk@users.noreply.github.com>
2025-04-03 17:29:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+document.querySelector(".cloaker").addEventListener("click", function() {
+    alert("Cloaker feature coming soon!");
+});
 
+function openGame(game) {
+    alert("Opening " + game + " (feature coming soon!)");
+}
